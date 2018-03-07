052040b9d9443b02c66738ca0e29f4815bed3c1e
Leif Swenson <leifswenson@leifs-mbp.lan>
2018-03-06 21:00:20 -0600

    more setup

---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Blue Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cowboy Burger');
